bbb3f4e037d497a20bfa13b6837090b18dc627af
StudioPassDE <cto@studiopass.de>
2019-04-01 22:14:29 +0200

    dynamic type

--- app.py
+++ app.py
@@ -1,12 +1,2 @@
 students_count = 1000
-rating = 4.99
-is_published = False
-course_name = "Python"
-multiple_lines = """
-multiple 
-lines
-"""
-
-x, y = 1, 2
-
-x = y = 1
+print(type(students_count))
